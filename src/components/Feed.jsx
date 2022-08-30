01af6569bbd5068f03fc8c7bb7bc352a73a3a5ab
Andresp08 <Andresfp29032001@gmail.com>
2022-08-30 15:53:48 -0500

    building feed and fetching data from API

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,17 @@
-
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import Sidebar from './Sidebar';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+import { Sidebar, Videos } from './';
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
       <Box
@@ -13,7 +21,10 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <Sidebar/>
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography
           className='copyright'
           variant='body2'
@@ -21,6 +32,18 @@ const Feed = () => {
         >
           Copyright {new Date().getFullYear()} Andrube Media
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overFlowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography
+          variant='h4'
+          fontWeight='bold'
+          mb={2}
+          sx={{ color: 'white' }}
+        >
+          {selectedCategory} <span style={{ color: '#F31503' }}>videos</span>
+        </Typography>
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
